1014936dda75a9ed120cb0d1d62d9cb31163a32a
Hafiz Mughees <mughees605@gmail.com>
2017-02-09 19:51:04 +0500

    adding

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from "./project_1";
+//import Menu from "./project-1/project_1";
+import Tooltip from "./project-2/project-2.js"
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 ReactDOM.render(
-  <Menu />,
+  <div>
+<Tooltip text = "Hafiz Mughees siddiqui">HMS</Tooltip>
+</div>,
   document.getElementById('root')
 );
