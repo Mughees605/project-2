74c8f00e6493742a99b4f934f4656cf7a1098d45
Hafiz Mughees <mughees605@gmail.com>
2017-02-10 15:49:40 +0500

    adding

--- project-3/src/index.js
+++ project-3/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import TimeWrapper from './components/project-3'
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+// component //
+// import Modal from "./component4/Modal"
+// import Checkout from "./component4/Checkout"
+// import Product from "./component4/Product"
+// import Cart from "./component4/Cart"
+// component end //
 ReactDOM.render(
-  <TimeWrapper />,
+  <TimeWrapper/>
+  ,
   document.getElementById('root')
 );
